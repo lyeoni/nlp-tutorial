cf5dc88482522c08ac371c5ddc8a1a8e4657dd2d
lyeoni <lyeoni.g@gmail.com>
2018-12-06 17:37:56 +0900

    update tokenization_ko.py

--- utils/tokenization_ko.py
+++ utils/tokenization_ko.py
@@ -1,27 +1,14 @@
-import pandas as pd
-import konlpy
+import sys, fileinput
 from konlpy.tag import Mecab
-import argparse
-
-def argparser():
-    p = argparse.ArgumentParser()
-
-    p.add_argument('--data',
-                   required=True,
-                   help='recommended file formats= .txt, .csv')
-
-    config = p.parse_args()
-
-    return config
 
 if __name__ == "__main__":
-    config = argparser()
-
-    corpus = pd.read_csv(config.data, error_bad_lines=False, warn_bad_lines=False)
-
-    # tokenizing
-    tokenizer = Mecab()
-    corpus['tokenization'] = [' '.join(tokenizer.morphs(i)) for i in corpus]
-
-    # save
-    corpus.to_csv('new_'+config.data, encoding='utf-8', index=False)
+    for line in fileinput.input():
+        if line.replace('\n', '').strip() != '':
+            # make tokenizer
+            tokenizer = Mecab()
+            
+            # tokenization
+            tokens = tokenizer.morphs(line.replace('\n', '').strip())
+            sys.stdout.write(' '.join(tokens) + '\n')
+        else:
+            sys.stdout.write('\n')
