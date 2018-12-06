096b59f96915d50c1f7958bd0c5130e25f50e54d
lyeoni <lyeoni.g@gmail.com>
2018-12-06 17:55:12 +0900

    add preprocessing.sh

---
+++ utils/preprocessing.sh
@@ -0,0 +1,7 @@
+# tokenization-ko
+python tokenization_ko.py < corpus.txt > corpus.tk.txt
+
+# get word embedding vectos based on pre-trained fasttext model
+# wiki.ko.bin is a binary file containing the parameters of the model along with the dictionary and all hyperparameters. The binary file can be used later to compute word vectors or to restart the optimization.
+cat corpus.tk.txt | /Users/hoyeonlee/fastText-0.1.0/fasttext print-word-vectors /Users/hoyeonlee/pretrained-word-vector/wiki.ko.bin > corpus.tk.vec.txt
+
