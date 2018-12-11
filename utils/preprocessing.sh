1eb0880799086ebf1094c667b49547897284e189
lyeoni <lyeoni.g@gmail.com>
2018-12-11 17:33:42 +0900

    update preprocessing.sh

--- utils/preprocessing.sh
+++ utils/preprocessing.sh
@@ -1,6 +1,9 @@
 # tokenization-ko
 python tokenization_ko.py < corpus.txt > corpus.tk.txt
 
+# optional: build vocabulary
+python build_vocab.py -input corpus.tk.txt -output corpus.tk.vocab.txt -word_num 12000
+
 # get word embedding vectos based on pre-trained fasttext model
 # wiki.ko.bin is a binary file containing the parameters of the model along with the dictionary and all hyperparameters.
 # The binary file can be used later to compute word vectors or to restart the optimization.
